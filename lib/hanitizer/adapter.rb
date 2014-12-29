3a356582fec2471c50706a956d57c73edbfa2d22
Dan Porter <wolfpakz@gmail.com>
2014-12-29 15:06:38 -0800

    Added abstract methods to Adapter
    Raise errors and complain about the methods not being implemented.

--- lib/hanitizer/adapter.rb
+++ lib/hanitizer/adapter.rb
@@ -11,5 +11,21 @@ module Hanitizer
         update(collection_name, entry['id'], updated_entry) unless updated_entry.eql?(entry)
       end
     end
+
+    def connect(url)
+      raise '%s has not implemented method connect(url)' % [self.class]
+    end
+
+    def collection_entries(collection)
+      raise '%s has not implemented method collection_entries(collection)' % [self.class]
+    end
+
+    def truncate(name)
+      raise '%s has not implemented method truncate(name)' % [self.class]
+    end
+
+    def update(collection_name, id, attributes)
+      raise '%s has not implemented method update(collection, id, attributes)' % [self.class]
+    end
   end
 end
